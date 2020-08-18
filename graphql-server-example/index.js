325c36db96d322227249102f6b867506d21a5da6
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-08-18 15:07:59 +0500

    making server

--- graphql-server-example/index.js
+++ graphql-server-example/index.js
@@ -1,15 +1,47 @@
 const { ApolloServer, gql } = require('apollo-server');
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
+const students = [
+    {
+        "id": 1,
+        "name": "Ali",
+        "email": "ali@gmail.com",
+        "age": 21
+    },
+    {
+        "id": 2,
+        "name": "Mohsin",
+        "email": "mohsin@gmail.com",
+        "age": 21
+    },
+    {
+        "id": 3,
+        "name": "Aamir",
+        "email": "aamir@gmail.com",
+        "age": 21
+    }
+]
+
+const resolvers = {
+    Query: {
+      students: () => students,
+    },
+  };
+
 const typeDefs = gql`
-  
-  type Book {
-    title: String
-    author: String
+  type Student {
+    id: Int
+    name: String
+    email: String
+    age: Int
   }
   type Query {
-    books: [Book]
+    students: [Student]
   }
 `;
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
