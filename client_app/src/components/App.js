b4259a635ec050075a16e674da9c13ca83157665
unais ali <51183141+unaisshazan@users.noreply.github.com>
2020-08-19 17:29:44 +0500

    api data fetching

--- client_app/src/components/App.js
+++ client_app/src/components/App.js
@@ -3,12 +3,12 @@ import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import './App.css';
 import client from '../config/gql_config';
-
+import Students from './Student'
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      
+      <Students/>
     </ApolloProvider>
   );
 }
